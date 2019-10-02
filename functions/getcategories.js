87d43e16f57fdeb94b4e1988ced1e38da7ccc743
Brian Ketelsen <bjk@BigBean-2.ketelsen.local>
2019-10-02 12:59:05 -0400

    adding netlify functions

--- functions/getcategories.js
+++ functions/getcategories.js
@@ -7,7 +7,7 @@ exports.handler = async (event, context) => {
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
-      body: data.values
+      body: data
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
