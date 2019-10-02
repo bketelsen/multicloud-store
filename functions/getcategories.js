6a7e95c91759b7da781358ec064caed7741cf7e2
Brian Ketelsen <bjk@BigBean-2.ketelsen.local>
2019-10-02 13:53:08 -0400

    adding netlify functions

--- functions/getcategories.js
+++ functions/getcategories.js
@@ -2,12 +2,14 @@ const fetch = require("node-fetch");
 
 const API_ENDPOINT = "https://sls-wus-dev-multicloud-product-demo-bjk.azurewebsites.net/api/categories";
 
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, callback) => {
   return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
     .then(response => response.json())
-    .then(data => ({
-      statusCode: 200,
-      body: data
-    }))
+    .then(data => callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          data: data
+        })
+      }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
