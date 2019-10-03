1fe39a4563be2a707b0d48de25364066b551d6bb
Brian Ketelsen <bjk@Brians-iMac-Pro.local>
2019-10-02 22:41:05 -0400

    who knows

--- src/api.js
+++ src/api.js
@@ -2,11 +2,11 @@ import categories from "./categorystore.js";
 async function getCategories() {
     fetch('/.netlify/functions/getcategories')
     .then(function(resp) {
-	    console.log(resp);
+
         return resp.json();
     })
     .then(function(data) {
-        const categories = data.values;
+        const categories = data.data.values;
       return categories;
     })
 }
